1b6c8fa8e0a491b549f22cdcd11972776c645016
Theekshana Hariscandra <47950733+wasthTheekshana@users.noreply.github.com>
2021-09-26 01:23:09 +0530

    Update input_page.dart
    create slides

--- lib/screen/input_page.dart
+++ lib/screen/input_page.dart
@@ -2,6 +2,8 @@ import 'package:bmi_calculator/components/icon_content.dart';
 import 'package:bmi_calculator/components/reuseable_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
@@ -12,6 +14,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  int height = 180;
+  int weight = 40;
+  int age = 10;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,6 +80,171 @@ class _InputPageState extends State<InputPage> {
                 //   child: null,
                 // ),
               ],
+            ),
+          ),
+          Expanded(
+            child: ReuseableCard(
+              colour: Color(0xFFFFFFFF),
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'height(cm)',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w900, //A45809
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250.0,
+                      ),
+                      Text(
+                        height.toString(),
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFA45809),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                        thumbColor: Color(0xFFBEA874),
+                        activeTrackColor: Color(0xFFA45809),
+                        inactiveTrackColor: Color(0xFFEFE4CA),
+                        overlayColor: Color(0xFFA45809),
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30.0)),
+                    child: Slider(
+                      onChanged: (double value) {
+                        setState(() {
+                          height = value.round();
+                        });
+                      },
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ReuseableCard(
+              colour: Color(0xFFFFFFFF),
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'weight(Kg)',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w900, //A45809
+                        ),
+                      ),
+                      SizedBox(
+                        width: 250.0,
+                      ),
+                      Text(
+                        weight.toString(),
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFA45809),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                        thumbColor: Color(0xFFBEA874),
+                        activeTrackColor: Color(0xFFA45809),
+                        inactiveTrackColor: Color(0xFFEFE4CA),
+                        overlayColor: Color(0xFFA45809),
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30.0)),
+                    child: Slider(
+                      onChanged: (double value) {
+                        setState(() {
+                          weight = value.round();
+                        });
+                      },
+                      value: weight.toDouble(),
+                      min: 20,
+                      max: 220.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ReuseableCard(
+              colour: Color(0xFFFFFFFF),
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Age',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w900, //A45809
+                        ),
+                      ),
+                      SizedBox(
+                        width: 300.0,
+                      ),
+                      Text(
+                        age.toString(),
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFA45809),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                        thumbColor: Color(0xFFBEA874),
+                        activeTrackColor: Color(0xFFA45809),
+                        inactiveTrackColor: Color(0xFFEFE4CA),
+                        overlayColor: Color(0xFFA45809),
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30.0)),
+                    child: Slider(
+                      onChanged: (double value) {
+                        setState(() {
+                          age = value.round();
+                        });
+                      },
+                      value: age.toDouble(),
+                      min: 10,
+                      max: 220.0,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
